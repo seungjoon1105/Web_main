488aa3219747f3a1558afd5ce6a1bbf31272ad81
admin <jsj9022@naver.com>
2025-04-30 10:56:49 +0900

    깃허브  9주차(쿠키)

--- js/js_popup.js
+++ js/js_popup.js
@@ -1,10 +1,47 @@
+function setCookie(name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/" + ";SameSite=None; Secure";
+    }
+
+    function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+        var cookie_array = cookie.split("; ");
+        
+        for ( var index in cookie_array) {
+        var cookie_name = cookie_array[index].split("=");
+        
+        if (cookie_name[0] == "popupYN") {
+            return cookie_name[1];
+            }
+        }
+    }
+  return ;
+}
+
+function closePopup() {
+    if (document.getElementById('check_popup').value) {
+    setCookie("popupYN", "N", 1);
+    console.log("쿠키를 설정합니다.");
+    self.close();
+    }
+}
+    
+        
+
 function pop_up() {
+    var cookieCheck = getCookie("popupYN");
+    if (cookieCheck != "N"){
     window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
+}
+
 // var close_time; // 시간 정보
-// var close_time2 = 10; // 10초 설정
+// var close_time2 = 50; // 10초 설정
 // clearTimeout(close_time); // 재호출 정지
-// close_time= setTimeout("close_window()", 10000);
+// close_time= setTimeout("close_window()", 50000);
 // // 1/1000 초 지정, 바로 시작
 // show_time(); // 실시간 시간 보여주기
 // function show_time(){
@@ -25,6 +62,7 @@ function show_clock(){
         msg += "오후";
         msg += currentDate.getHours()-12+"시";
         }
+       
         else {
         msg += "오전";
         msg += currentDate.getHours()+"시";
@@ -32,6 +70,7 @@ function show_clock(){
         msg += currentDate.getMinutes()+"분";
         msg += currentDate.getSeconds()+"초";
         divClock.innerText = msg;
+       
         if (currentDate.getMinutes()>58) { //정각 1분전 빨강색 출력
         divClock.style.color="red";
         }
